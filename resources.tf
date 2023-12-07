a9f62a64a99c9e7f51f27c1244aa28e3c050acca
vikas <vikasbnarikkady@gmail.com>
2023-12-07 15:27:25 +0000

    remote access security group created

--- resources.tf
+++ resources.tf
@@ -36,3 +36,27 @@ resource "aws_security_group" "frontend_access" {
     Name = "${var.project_name}-${var.project_env}-frontend_access"
   }
 }
+
+resource "aws_security_group" "remote_access" {
+  name = "${var.project_name}-${var.project_env}-remote_access"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-remote_access"
+  }
+}
