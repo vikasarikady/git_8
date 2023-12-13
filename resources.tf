43980424b4ab4f42e04fa8f1b1c2a3cdc59bbd5c
vikas-bn-1 <vikasbnarikkady1@gmail.com>
2023-12-13 17:53:46 +0000

    premetheus port enabled

--- resources.tf
+++ resources.tf
@@ -31,6 +31,14 @@ resource "aws_security_group" "frontend_access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+    
+ ingress {
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
